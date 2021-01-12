7a4c3418ecc0a1034e496abfff9e35c0604948ff
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-01-11 23:05:43 -0800

    Add GKE configuration

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,65 +1,43 @@
-provider "kubernetes" {}
-
-resource "kubernetes_deployment" "nginx" {
-  metadata {
-    name = "scalable-nginx-example"
-    labels = {
-      App = "ScalableNginxExample"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.52.0"
     }
-  }
-
-  spec {
-    replicas = 4
-    selector {
-      match_labels = {
-        App = "ScalableNginxExample"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          App = "ScalableNginxExample"
-        }
-      }
-      spec {
-        container {
-          image = "nginx:1.7.8"
-          name  = "example"
-
-          port {
-            container_port = 80
-          }
-
-          resources {
-            limits {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-        }
-      }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "1.13.3"
     }
   }
 }
 
-resource "kubernetes_service" "nginx" {
-  metadata {
-    name = "nginx-example"
-  }
-  spec {
-    selector = {
-      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
-    }
-    port {
-      node_port   = 30201
-      port        = 80
-      target_port = 80
-    }
+data "terraform_remote_state" "gke" {
+  backend = "local"
 
-    type = "NodePort"
+  config = {
+    path = "../learn-terraform-provision-gke-cluster/terraform.tfstate"
   }
+}
+
+# Retrieve GKE cluster information
+provider "google" {
+  project = data.terraform_remote_state.gke.outputs.project_id
+  region  = data.terraform_remote_state.gke.outputs.region
+}
+
+# Retrieve an access token as the Terraform runner
+data "google_client_config" "provider" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = data.terraform_remote_state.gke.outputs.kubernetes_cluster_name
+  location = data.terraform_remote_state.gke.outputs.region
+}
+
+provider "kubernetes" {
+  load_config_file = false
+
+  host = data.terraform_remote_state.gke.outputs.kubernetes_cluster_host
+
+  token                  = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
 }
