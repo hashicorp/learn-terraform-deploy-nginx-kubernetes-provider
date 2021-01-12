acdafd1160ff1acc632dd7ac67b29227223fd6c7
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-01-11 23:06:33 -0800

    Add GKE configuration

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,65 +1,40 @@
-provider "kubernetes" {}
-
-resource "kubernetes_deployment" "nginx" {
-  metadata {
-    name = "scalable-nginx-example"
-    labels = {
-      App = "ScalableNginxExample"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.42.0"
     }
-  }
-
-  spec {
-    replicas = 4
-    selector {
-      match_labels = {
-        App = "ScalableNginxExample"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          App = "ScalableNginxExample"
-        }
-      }
-      spec {
-        container {
-          image = "nginx:1.7.8"
-          name  = "example"
-
-          port {
-            container_port = 80
-          }
-
-          resources {
-            limits {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-        }
-      }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "1.13.3"
     }
   }
 }
 
-resource "kubernetes_service" "nginx" {
-  metadata {
-    name = "nginx-example"
-  }
-  spec {
-    selector = {
-      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
-    }
-    port {
-      node_port   = 30201
-      port        = 80
-      target_port = 80
-    }
+data "terraform_remote_state" "aks" {
+  backend = "local"
 
-    type = "NodePort"
+  config = {
+    path = "../learn-terraform-provision-aks-cluster/terraform.tfstate"
   }
+}
+
+# Retrieve AKS cluster information
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_kubernetes_cluster" "cluster" {
+  name                = data.terraform_remote_state.aks.outputs.kubernetes_cluster_name
+  resource_group_name = data.terraform_remote_state.aks.outputs.resource_group_name
+}
+
+provider "kubernetes" {
+  load_config_file = false
+
+  host = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
 }
