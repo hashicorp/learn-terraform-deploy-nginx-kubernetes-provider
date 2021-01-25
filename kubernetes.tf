6f437e0b954a02a60a1d25798eba0fff762b2361
Tu Nguyen <im2nguyen@users.noreply.github.com>
2021-01-25 10:33:31 -0800

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -33,7 +33,7 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws"
     args = [
       "eks",
