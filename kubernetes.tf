f442e7e6aafad21cf67665debee247ed86d0a2d1
Tu Nguyen <im2nguyen@gmail.com>
2022-03-31 22:50:36 -0700

    Upgrade to AzureRM 3.0

--- kubernetes.tf
+++ kubernetes.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.42.0"
+      version = "3.0.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
